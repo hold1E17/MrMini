5fa9853f79d45f1b8e5c80dd27a81c3998eb3236
trules <troelsjc@gmail.com>
2017-10-02 11:13:35 +0200

    Initial commit

---
+++ app/src/main/java/mrmini/hold1e17/dk/mrmini/MainActivity.java
@@ -0,0 +1,13 @@
+package mrmini.hold1e17.dk.mrmini;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
