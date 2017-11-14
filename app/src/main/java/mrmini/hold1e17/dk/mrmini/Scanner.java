a635c59bb550861921a452a4da55f49022bd662e
piilmann <christoffer@korn8.dk>
2017-11-14 10:39:09 +0100

    Merge master + YouTube API

--- app/src/main/java/mrmini/hold1e17/dk/mrmini/Scanner.java
+++ app/src/main/java/mrmini/hold1e17/dk/mrmini/Scanner.java
@@ -1,13 +1,36 @@
 package mrmini.hold1e17.dk.mrmini;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Scanner extends AppCompatActivity {
+public class Scanner extends AppCompatActivity implements View.OnClickListener {
+
+    Button scanBut1, scanBut2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
+
+        scanBut1 = (Button) findViewById(R.id.scanBut1);
+        scanBut2 = (Button) findViewById(R.id.scanBut2);
+
+        scanBut1.setOnClickListener(this);
+        scanBut2.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == scanBut1) {
+            Intent i = new Intent(this, Scanner_Toy.class);
+            startActivity(i);
+        } else if (v == scanBut2) {
+
+        }
     }
 }
