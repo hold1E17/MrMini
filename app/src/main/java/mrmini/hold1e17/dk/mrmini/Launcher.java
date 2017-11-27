0833da9623bebaf2c8439641fac49cb103b6c0bf
Simon <s.lundorf@gmail.com>
2017-11-27 12:03:49 +0100

    Begun work on the login and the launcher. Saving info through the preference manager. Passing data to activity through intents.

--- app/src/main/java/mrmini/hold1e17/dk/mrmini/Launcher.java
+++ app/src/main/java/mrmini/hold1e17/dk/mrmini/Launcher.java
@@ -1,7 +1,9 @@
 package mrmini.hold1e17.dk.mrmini;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.Window;
@@ -12,12 +14,21 @@ import android.view.Window;
 
 public class Launcher extends AppCompatActivity {
 
+    String login = "";
+    String hospital = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (savedInstanceState == null) {
+        loadData();
+        System.out.println(login);
+        System.out.println(hospital);
+
+        if (login == "" && hospital == "") {
             Intent i = new Intent(this, Login.class);
+            i.putExtra("login", login);
+            i.putExtra("hospital", hospital);
             startActivity(i);
         } else {
             Intent i = new Intent(this, Hovedmenu.class);
@@ -26,6 +37,16 @@ public class Launcher extends AppCompatActivity {
 
         setTitle("MR Scanner");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    public void loadData() {
+        // Function that loads the data stored in the SharedPreferences
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        login = preferences.getString("login", "");
+        hospital = preferences.getString("hospital", "");
 
     }
 
