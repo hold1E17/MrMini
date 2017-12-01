2a261ab9ee8196a922b173f3272d0b69c4f508e7
sofiefreja <spiegel1>
2017-12-01 05:34:43 +0100

    Trial to work on oreo

--- app/src/main/java/mrmini/hold1e17/dk/mrmini/Scanner_app.java
+++ app/src/main/java/mrmini/hold1e17/dk/mrmini/Scanner_app.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
@@ -16,13 +17,15 @@ import java.io.File;
  * Created by sofie on 26-11-2017.
  */
 
-public class Scanner_app extends Activity{
+public class Scanner_app extends Activity {
     Button button;
     ImageView imageView;
     static final int CAM_REQUEST = 1;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
         setContentView(R.layout.activity_scanner_app);
         button = (Button) findViewById(R.id.button);
         imageView = (ImageView) findViewById(R.id.image_view);
@@ -35,8 +38,8 @@ public class Scanner_app extends Activity{
             }
         });
         button.setText("Tag billede");
-
     }
+
     private File getFile() {
         File folder = new File("sdoard/camera_app");
         if(!folder.exists()) {
@@ -52,4 +55,6 @@ public class Scanner_app extends Activity{
         String path = "sdoard/camera_app/cam_image.jpg";
         imageView.setImageDrawable(Drawable.createFromPath(path));
     }
+
+
 }
