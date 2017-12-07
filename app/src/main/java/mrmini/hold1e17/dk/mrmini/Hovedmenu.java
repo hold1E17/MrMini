3f48625ec2ae3a1030a6672a32b7d68beb8140e4
Simon <s.lundorf@gmail.com>
2017-12-07 14:17:47 +0100

    Implemented a preference.xml file to store preferences. Added preferences for hospital and one for muting the app. Implemented them in a settings activity.

--- app/src/main/java/mrmini/hold1e17/dk/mrmini/Hovedmenu.java
+++ app/src/main/java/mrmini/hold1e17/dk/mrmini/Hovedmenu.java
@@ -2,12 +2,10 @@ package mrmini.hold1e17.dk.mrmini;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
