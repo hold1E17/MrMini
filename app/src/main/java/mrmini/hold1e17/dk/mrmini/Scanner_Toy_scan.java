9366abf3c978d0b0912cb29f08798f1fd87e64ea
piilmann <christoffer@korn8.dk>
2018-01-12 11:50:18 +0100

    Ny skærm med switch case

---
+++ app/src/main/java/mrmini/hold1e17/dk/mrmini/Scanner_Toy_scan.java
@@ -0,0 +1,91 @@
+package mrmini.hold1e17.dk.mrmini;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.content.ContentValues.TAG;
+
+public class Scanner_Toy_scan extends AppCompatActivity {
+
+    private static Context context;
+    private static TextView statusTextScan;
+    private static ImageView scannerImg;
+
+    private static String savedCase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner__toy_scan);
+        statusTextScan = (TextView) findViewById(R.id.textStatusScan);
+        scannerImg = (ImageView) findViewById(R.id.scanner_empty);
+
+        if(savedInstanceState != null){
+            savedCase = savedInstanceState.getString("savedCase");
+            if(savedCase != null){
+                read(savedCase);
+            }
+        }
+    }
+
+    public static void read(String messageRead) {
+
+        switch (messageRead) {
+            case "1":
+                statusTextScan.setText("Dukken er blevet fjernet, og derfor afbrydes scanningen");
+                scannerImg.setImageResource(R.drawable.scanner);
+                savedCase = "1";
+                break;
+            case "2":
+                statusTextScan.setText("Ingen dukke");
+                scannerImg.setImageResource(R.drawable.scanner);
+                savedCase = "2";
+                break;
+            case "3":
+                statusTextScan.setText("Dukken er placeret korrekt");
+                scannerImg.setImageResource(R.drawable.scanner_mand);
+                savedCase = "3";
+                break;
+            case "4":
+                statusTextScan.setText("MR scanneren er ved at bevæge sig ind");
+                scannerImg.setImageResource(R.drawable.scanner_mand);
+                savedCase = "4";
+                break;
+            case "5":
+                statusTextScan.setText("MR scanneren er ved at bevæge sig ud");
+                scannerImg.setImageResource(R.drawable.scanner_mand);
+                savedCase = "5";
+                break;
+            case "6":
+                statusTextScan.setText("Scanningen er igang");
+                savedCase = "6";
+                break;
+            case "7":
+                statusTextScan.setText("Dukken der er blevet placeret er en dreng");
+                scannerImg.setImageResource(R.drawable.scanner_mand);
+                savedCase = "7";
+                break;
+            case "8":
+                statusTextScan.setText("Dukken der er blevet placeret er en pige");
+                savedCase = "8";
+                break;
+            case "9":
+                statusTextScan.setText("Dukken der er blevet placeret er en skildpadde");
+                savedCase = "9";
+                break;
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("savedCase", savedCase);
+        super.onSaveInstanceState(outState);
+    }
+}
