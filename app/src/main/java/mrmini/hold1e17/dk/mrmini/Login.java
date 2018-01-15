edfac4ab4bf1b4bcd41ee17c8a07c405ae87ee32
Simon <s.lundorf@gmail.com>
2018-01-15 11:33:12 +0100

    Cleaning up code. Creating logic classes.

--- app/src/main/java/mrmini/hold1e17/dk/mrmini/Login.java
+++ app/src/main/java/mrmini/hold1e17/dk/mrmini/Login.java
@@ -1,10 +1,8 @@
 package mrmini.hold1e17.dk.mrmini;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.AnimationUtils;
--- app/src/main/java/mrmini/hold1e17/dk/mrmini/Scanner.java
+++ app/src/main/java/mrmini/hold1e17/dk/mrmini/Scanner.java
@@ -18,10 +18,10 @@ public class Scanner extends AppCompatActivity implements View.OnClickListener {
         setContentView(R.layout.activity_scanner);
         overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
 
-        scanBut1 = (Button) findViewById(R.id.scanBut1);
-        scanBut2 = (Button) findViewById(R.id.scanBut2);
-        sygeplejeske1 = (Button) findViewById(R.id.krop1);  // Skal ændres
-        hoved1 = (Button) findViewById(R.id.hoved1);
+        scanBut1 = findViewById(R.id.scanBut1);
+        scanBut2 = findViewById(R.id.scanBut2);
+        sygeplejeske1 = findViewById(R.id.krop1);  // Skal ændres
+        hoved1 = findViewById(R.id.hoved1);
 
         scanBut1.setOnClickListener(this);
         scanBut2.setOnClickListener(this);
